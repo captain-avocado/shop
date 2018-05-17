f4946e6b5382d5d83f908f4281efdcdf5d373090
Artem Kaloev <kaloev.eltech@gmail.com>
2018-05-17 20:19:09 +0300

    updated search-row

--- src/scripts/modules/search.js
+++ src/scripts/modules/search.js
@@ -1,6 +1,49 @@
 export default function search() {
 
-    const searchIcon = document.getElementById('search');
-    console.log(searchIcon);
+    const searchIcon = document.getElementById('open');
+    const menu = document.querySelector('.menu');
+    const searchRow = document.querySelector('.search-row');
+    const closeIcon = document.getElementById('close');
+
+    const searchBtn = document.getElementById('search');
+
+    searchIcon.addEventListener('click', e => {
+        e.preventDefault();
+
+        menu.style.opacity = 0;
+        searchRow.style.opacity = 1;
+
+        menu.addEventListener('transitionend', e => {
+            searchIcon.style.display = 'none';
+            menu.style.display = 'none';
+
+            closeIcon.style.display = 'block';
+            searchRow.style.display = 'flex';
+            searchRow.style.opacity = 1;
+
+        });
+
+        console.log('Открытие строки...');
+
+    });
+
+    closeIcon.addEventListener('click', e => {
+        e.preventDefault();
+
+        searchRow.style.opacity = 0;
+        menu.style.opacity = 1;
+
+        searchRow.addEventListener('transitionend', e => {
+            searchRow.style.display = '';
+            searchIcon.style.display = '';
+            menu.style.display = '';
+            closeIcon.style.display = '';
+        });
+
+        console.log('Закрытие строки...');
+
+    });
+
+    searchBtn.addEventListener('click', e => e.preventDefault());
 
 }
