e60c749d038e4807a916c8fe15fcb34abc7bd746
Artem Kaloev <kaloev.eltech@gmail.com>
2018-05-19 01:18:33 +0300

    updated slider

--- src/scripts/modules/slider.js
+++ src/scripts/modules/slider.js
@@ -1,5 +1,7 @@
 export default function slider() {
 
+    let i = 0;
+
     const sliderList = document.querySelector('.slider__list');
     const sliderDots = document.querySelectorAll('.dots__item');
 
@@ -7,16 +9,80 @@ export default function slider() {
         sliderList.style.left = `-${num}00%`;
     }; 
 
-    sliderDots.forEach((item, i) => {
+    sliderDots.forEach((item, index) => {
         item.addEventListener('click', (e) => {
             e.preventDefault();
-            
+
             const lastActiveItem = Array.from(sliderDots).filter(item => item.classList.contains('active'));
             lastActiveItem[0].classList.remove('active');
-
+            i = index;
             item.classList.add('active');
-            moveSlide(i);
+            moveSlide(index);
         });
     });
+
+    const changeActiveItem = (newActiveItem) => {
+        const lastActiveItem = Array.from(sliderDots).filter(item => item.classList.contains('active'));
+        lastActiveItem[0].classList.remove('active');
+        newActiveItem.classList.add('active');
+    };
+    
+    const sliderImages = document.querySelector('.slider__images');
+    // sliderImages.addEventListener('mousedown', e => {
+    //     console.log(e.clientX);
+    // });
+
+    let directionOffsetX = 0;
+    let currentOffsetX = 0;
+    
+    const dragMouseDown = e => {
+      
+        const closeDragElement = () => {
+            document.onmouseup = null;
+            document.onmousemove = null;
+        };
+
+        const elementDrag = (e) => {
+            directionOffsetX = currentOffsetX - e.clientX;
+            currentOffsetX = e.clientX;
+            let sliderOffset = sliderList.offsetLeft;
+
+            // console.log('---');
+            if ((sliderOffset >= 0 && directionOffsetX < 0) || (sliderOffset <= -3600 && directionOffsetX > 0)) {
+                // console.log(sliderOffset);
+                // console.log(directionOffsetX);
+                closeDragElement();
+            } else {
+                let sliderCurrentOffset = (sliderList.offsetLeft - directionOffsetX*40);
+                
+                if ((sliderCurrentOffset <= 0) && (sliderCurrentOffset >= -3600)) {
+                    // console.log(sliderCurrentOffset);
+                    // console.log(-(i * 900 + 450));
+                    if (sliderCurrentOffset < -(i * 900 + 450)) {
+                        i++;
+                        sliderCurrentOffset = -(i * 900);
+                        changeActiveItem(sliderDots[i]);
+                        closeDragElement();
+                    } else if (sliderCurrentOffset > -(i * 900 - 450)) {
+                        i--;
+                        sliderCurrentOffset = -(i * 900);
+                        changeActiveItem(sliderDots[i]);
+                        closeDragElement();
+                    }
+
+                    sliderList.style.left = sliderCurrentOffset + 'px';
+                } else console.log((sliderList.offsetLeft - directionOffsetX*40));
+            }
+        };
+
+        currentOffsetX = e.clientX;
+        document.onmouseup = () => {
+            sliderList.style.left = -(i * 900) + 'px';
+            closeDragElement();
+        };
+        document.onmousemove = elementDrag;
+
+    };
+    sliderImages.onmousedown = dragMouseDown;
 
 }
